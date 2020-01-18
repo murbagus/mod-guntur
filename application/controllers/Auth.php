c4a69f9b29dc6cd2dffd32b237fddacfb45bb32e
guntur27 <gunturpratama210@gmail.com>
2020-01-18 14:10:39 +0700

    ganti base url ke laragon
    ganti base url

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -60,7 +60,7 @@ class Auth extends CI_Controller
                 }
             } else {
                 $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
-            Username ini belum aktif! </div>');
+                Username ini belum aktif! </div>');
                 redirect('auth');
             }
         } else {
@@ -70,42 +70,42 @@ class Auth extends CI_Controller
         }
     }
 
-    // public function registration()
-    // {
-    //     if ($this->session->userdata('email')) {
-    //         redirect('user');
-    //     }
+    public function registration()
+    {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
 
-    //     $this->form_validation->set_rules('name', 'Name', 'required|trim');
-    //     $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-    //         'is_unique' => 'This email has already registered!'
-    //     ]);
-    //     $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
-    //     $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
-    //     if ($this->form_validation->run() == false) {
+        if ($this->form_validation->run() == false) {
 
-    //         $data['title'] = 'User Registration';
-    //         $this->load->view('templates/auth_header', $data);
-    //         $this->load->view('auth/registration');
-    //         $this->load->view('templates/auth_footer');
-    //     } else {
-    //         $data = [
-    //             'name' => htmlspecialchars($this->input->post('name', true)),
-    //             'email' => htmlspecialchars($this->input->post('email', true)),
-    //             'image' => 'default.jpg',
-    //             'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-    //             'role_id' => 1,
-    //             'is_active' => 1,
-    //             'date_created' => time()
-    //         ];
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 1,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
 
-    //         $this->db->insert('user', $data);
-    //         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
-    //         Congratulation! Your account has been created. Please Login </div>');
-    //         redirect('auth');
-    //     }
-    // }
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulation! Your account has been created. Please Login </div>');
+            redirect('auth');
+        }
+    }
 
     public function logout()
     {
